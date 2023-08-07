9f9aed5891226049668854b8b60df105027a5c4b
Alejandro <alejandrotomevlle@gmail.com>
2023-08-07 13:50:33 +0200

    beta menu

--- src/tsx/Menu.tsx
+++ src/tsx/Menu.tsx
@@ -128,7 +128,7 @@ function ResponsiveAppBar() {
               </Button>
             ))}
           </Box>
-
+{/* Desplegable */}
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
