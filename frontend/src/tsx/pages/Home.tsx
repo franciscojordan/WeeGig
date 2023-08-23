3c94b7043e9fc2a071b1bb795d1d1ec70caa5f9b
Alejandro <alejandrotomevlle@gmail.com>
2023-08-23 12:43:26 +0200

    things

--- frontend/src/tsx/pages/Home.tsx
+++ frontend/src/tsx/pages/Home.tsx
@@ -23,7 +23,7 @@ function DivBox(props) {
 
 function Home() {
     // Simulando que el usuario está registrado (cambiar a true o false según corresponda)
-    const userIsRegistered = false;
+    const user = true;
 
     return (
         <div>
@@ -32,7 +32,7 @@ function Home() {
                     <h1 className="big">¡Bienvenidos a WEE GIG!</h1>
                     <p>En WEE GIG, nos enorgullece ser un puente entre individuos comprometidos y apasionados por hacer una diferencia en el mundo y oportunidades de trabajo en el ámbito de la beneficencia.</p>
                     {/* Condición para mostrar o no los botones */}
-                    {!userIsRegistered && (
+                    {!user && (
                         <div>
                             <a href="/registrar"><Button value="Crear Cuenta" type="green" /></a>
                             <a href="/Login"><Button value="Iniciar Sesión" /></a>
