3de87c939e0af3177aac579f9e69851cae8e9a22
Victor Aguilar <veaz.24@gmail.com>
2023-08-24 13:34:25 +0200

    cleanin directory

--- credentials.js
+++
@@ -1,7 +0,0 @@
-// credentials.js
-const credentials = {
-    apiKey: 'AIzaSyCE41J5o571OE-TOkL9XBvlWzZ5sZEMh5w',
-    // Add more credentials or configuration here
-};
-
-export default credentials;
